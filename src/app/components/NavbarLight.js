1faaa29766bc6b05e11dd86b6e5570995f8a69e0
Murtaza445 <murtazajohar123@gmail.com>
2025-09-08 11:57:20 +0500

    changes

--- src/app/components/NavbarLight.js
+++ src/app/components/NavbarLight.js
@@ -23,10 +23,10 @@ export default function NavbarLight(){
         <div className="container flex flex-wrap items-center justify-between">
             <Link href="/" className="navbar-brand md:me-8" >
                 <span className="inline-block dark:hidden">
-                    <Image width={96} height={20} src="/images/logo-dark.png" className="l-dark" alt=""/>
-                    <Image width={96} height={20} src="/images/logo-light.png" className="l-light" alt=""/>
+                    <Image width={144} height={30} src="/images/logo-black.png" className="l-dark" alt=""/>
+                    <Image width={144} height={30} src="/images/logo-white.png" className="l-light" alt=""/>
                 </span>
-                <Image width={96} height={20} src="/images/logo-light.png" className="hidden dark:inline-block" alt=""/>
+                <Image width={144} height={30} src="/images/logo-white.png" className="hidden dark:inline-block" alt=""/>
             </Link>
             
 
@@ -35,16 +35,16 @@ export default function NavbarLight(){
                 <ul className="list-none menu-social mb-0">
                     <li className="inline-flex">
                         <Link href="" className="ms-1">
-                            <span className="login-btn-primary"><span className="btn btn-icon btn-sm rounded-full bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white"><Unicons.UilDribbble width={16}/></span></span>
+                            <span className="login-btn-primary"><span className="btn btn-icon btn-sm rounded-full bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white"><Unicons.UilInstagram width={16}/></span></span>
+                            <span className="login-btn-light"><span className="btn btn-icon btn-sm rounded-full bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white"><Unicons.UilInstagram width={16}/></span></span>
+                        </Link>
+                        <Link href="" className="ms-1">
+                            <span className="login-btn-primary"><span className="btn btn-icon btn-sm rounded-full bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white"><Unicons.UilFacebookF width={16}/></span></span>
                             <span className="login-btn-light"><span className="btn btn-icon btn-sm rounded-full bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white"><Unicons.UilDribbble width={16}/></span></span>
                         </Link>
                         <Link href="" className="ms-1">
-                            <span className="login-btn-primary "><span className="btn btn-icon btn-sm rounded-full bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white"><Unicons.UilBehance width={16}/></span></span>
+                            <span className="login-btn-primary "><span className="btn btn-icon btn-sm rounded-full bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white"><Unicons.Linkedin width={16}/></span></span>
                             <span className="login-btn-light "><span className="btn btn-icon btn-sm rounded-full bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white"><Unicons.UilBehance width={16}/></span></span>
-                        </Link>
-                        <Link href="" className="ms-1">
-                            <span className="login-btn-primary"><span className="btn btn-icon btn-sm rounded-full bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white"><Unicons.UilInstagram width={16}/></span></span>
-                            <span className="login-btn-light"><span className="btn btn-icon btn-sm rounded-full bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white"><Unicons.UilInstagram width={16}/></span></span>
                         </Link>
                     </li>
                 </ul>
